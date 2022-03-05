6d0dbb20b223b09ef15eba25593b2a60b1fcdd10
Corey Thompson <corey.dwayne.thompson@gmail.com>
2022-03-05 07:22:26 -0600

    release notes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,22 +12,43 @@
 		</div>
 	</h1>
 
+	<div class="focus">
+		<h1>Version - Preview 0.1</h1>
+		<ul>
+			<li><h2>View your upcoming due orders on your <strong>schedule</strong></h2></li>
+			<li><h2>Forecast your labor allocation after extracting bom labor and using <strong>labor</strong></h2></li>
+			<li><h2>Toggle your <strong>detailed view</strong> in order to further inspect tabular data being viewed</h2></li>
+		</ul>
+	</div>
+
+	<div class="focus">
+		<h2>Upcoming Features</h2>
+		<ul>
+			<li><h2>A labor chart that actually functions</h2></li>
+			<li><h2>Edit number of days into future to forecast</h2></li>
+			<li><h2>Port token expiration notice</h2></li>
+			<li><h2>User login and authentication</h2></li>
+			<li><h2>Automatic token refresh</h2></li>
+			<li><h2>Improved chart sizing</h2></li>
+			<li><h2>Filter data using tables</h2></li>
+		</ul>
+	</div>
+
+	<h2>Change Log</h2>
 	<ul>
 		<li>
-
-			<h2>
-				View your upcoming due orders on your <strong>schedule</strong>
-			</h2>
-		</li>
-		<li>
-			<h2>
-				Forecast your labor allocation after extracting bom labor and using <strong>labor</strong>
-			</h2>
-		</li>
-		<li>
-			<h2>
-				Toggle your <strong>detailed view</strong> in order to further inspect tabular data being viewed
-			</h2>
+			<h2>Preview 0.1 - Initial Preview</h2>
+			<ul>
+				<li>Toggle detailed data</li>
+				<li>Extract labor value from open work orders</li>
+				<li>Add page routing and nav bar</li>
+				<li>Add notification system</li>
+				<li>Share data across template pages</li>
+				<li>Add typescript support and models for SOS items</li>
+				<li>Create SOS data client</li>
+				<li>Port timeline from script import to webcomponent</li>
+				<li>Port tables from script import to webcomponent</li>
+			</ul>
 		</li>
 	</ul>
 
@@ -37,8 +58,6 @@
 	section {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
-		align-items: center;
 		flex: 1;
 	}
 
@@ -47,7 +66,7 @@
 	}
 
 	li{
-		margin: 2rem;
+		margin: 1rem;
 	}
 	.welcome {
 		position: relative;
@@ -62,5 +81,13 @@
 		height: 100%;
 		top: 0;
 		display: block;
+	}
+
+	ul, li{
+		justify-content: left;
+	}
+
+	.focus{
+		margin-bottom: 4rem;
 	}
 </style>
