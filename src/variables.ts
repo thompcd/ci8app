64212a6d9cb47769b8b9d9e237472a656d810124
Corey Thompson <corey.dwayne.thompson@gmail.com>
2022-03-05 08:01:32 -0600

    change env lookup

--- src/variables.ts
+++ src/variables.ts
@@ -1,4 +1,4 @@
 export const variables = {
-    saddleOakBasePath: import.meta.env.VITE_SADDLE_OAK_BASE_PATH,
-    saddleOakToken: import.meta.env.VITE_SADDLE_OAK_TOKEN
+    saddleOakBasePath: import.meta.env['VITE_SADDLE_OAK_BASE_PATH'],
+    saddleOakToken: import.meta.env['VITE_SADDLE_OAK_TOKEN']
   };
