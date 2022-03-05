29c4b2b82e6755d3755d127ad0fd8f497c702b7a
Corey Thompson <corey.dwayne.thompson@gmail.com>
2022-03-05 08:22:41 -0600

    hacky env attempt

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import path from 'path'
+import vue from '@vitejs/plugin-vue'
+
+const viteEnv = {} Object.keys(process.env).forEach((key) => { if (key.startsWith(VITE_)) { viteEnv[import.meta.env.${key}] = process.env[key] } })
+
+export default { alias: { '@': require('path').resolve(__dirname, 'src') }, define: viteEnv, plugins: [ vue() ] }
